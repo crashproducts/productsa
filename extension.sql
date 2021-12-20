f61a09001eeba4520e5f8f9161b514825761252d
crashproducts <95462972+crashproducts@users.noreply.github.com>
2021-12-20 07:54:29 +0400

    Add files via upload

---
+++ extension.sql
@@ -0,0 +1,22 @@
+CREATE TABLE users(
+    steam64id bigint,
+    avatar VARCHAR(43),
+    PRIMARY KEY(steam64id)
+);
+
+CREATE INDEX avatar_index ON users (avatar);
+
+
+CREATE TABLE listings(
+    id SERIAL PRIMARY KEY,
+    game int,
+    item_name VARCHAR(64),
+    time timestamp,
+    price int,
+    owner_name VARCHAR(64),
+    owner_avatar VARCHAR(43),
+    CONSTRAINT owner_item_constraint UNIQUE(owner_name, item_name)
+);
+
+CREATE INDEX ON listings(owner_name);
+CREATE INDEX ON listings(item_name);
